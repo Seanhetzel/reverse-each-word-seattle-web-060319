8e327ad2a36430716d33295b41ac7e7530c07e4f
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-17 03:56:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
-  reversed_string = []
+  #reversed_string = []
   string.split(" ").collect do |word|
-    reversed_string << word.reverse
+    #reversed_string << word.reverse
+    word.reverse.join(" ")
   end
-  reversed_string.join(" ")
+  #reversed_string.join(" ")
 end
